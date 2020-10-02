1b3a18cb1fe3acd59ba50c8e3af5de36b6f6bfdd
lowrylab-cuboulder <34254263+lowrylab-cuboulder@users.noreply.github.com>
2020-10-01 23:52:18 -0400

    Added test images and local changes to file paths
    blah

--- image_adjustment.py
+++ image_adjustment.py
@@ -4,9 +4,10 @@ from keras.preprocessing.image import load_img, img_to_array
 def predict(model, directory):
 
     img_array = img_to_array(load_img(directory, color_mode = "grayscale"))/255
+    img_array = img_array[0:(img_array.shape[0]-img_array.shape[0]%16), 0:(img_array.shape[1]-img_array.shape[1]%16)] # minimal cropping
     data = img_array.reshape(-1, img_array.shape[0], img_array.shape[1], img_array.shape[2])
     
-    pred = model.predict(data)
+    pred = model.predict(data, verbose=1)
     pred = np.where(pred > 0.5, 1, 0)
 
     return pred.reshape(img_array.shape[0], img_array.shape[1], 1)
--- parse_functions.py
+++ parse_functions.py
@@ -6,4 +6,5 @@ from keras.preprocessing.image import save_img
 def predict_parser_func(args):
     model = load_model("model")
     pred = predict(model, args.img_path)
-    save_img("C:\\Users\\rishi\\Desktop\\img.png", pred)
+    #save_img("C:\\Users\\rishi\\Desktop\\img.png", pred)
+    save_img("D:\\Dropbox (SBG)\\David-Smith\\Jefferson-Desktop\\Code\\EM_hepatocyte\\output\\img.png", pred)
